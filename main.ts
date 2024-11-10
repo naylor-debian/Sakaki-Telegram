a76b1cc805e1e4995e21cd7476839c6d5427408e
naylor-debian <95220392+naylor-debian@users.noreply.github.com>
2024-11-10 11:11:30 -0300

    Add files via upload

--- main.ts
+++ main.ts
@@ -1,1 +1,22 @@
+const { apiId, apiHash, } = require ('./config.json'); 
+const { TelegramClient } = require("telegram");
+const { StringSession } = require("telegram/sessions");
+const input = require("input"); // npm i input
+const stringSession = new StringSession(""); // fill this later with the value from session.save()
 
+(async () => {
+  console.log("Loading interactive example...");
+  const client = new TelegramClient(stringSession, apiId, apiHash, {
+    connectionRetries: 5,
+  });
+  await client.start({
+    phoneNumber: async () => await input.text("Please enter your number: "),
+    password: async () => await input.text("Please enter your password: "),
+    phoneCode: async () =>
+      await input.text("Please enter the code you received: "),
+    onError: (err: any) => console.log(err),
+  });
+  console.log("You should now be connected.");
+  console.log(client.session.save()); // Save this string to avoid logging in again
+  await client.sendMessage("me", { message: "Hello!" });
+})();
